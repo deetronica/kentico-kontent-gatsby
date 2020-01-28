8cb82ce632b2280a5bdd02d1274ef5157818e4d7
Maurice <mmahandev@users.noreply.github.com>
2020-01-28 14:19:08 -0600

    Re-trigger checks

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default ({ data }) => {
       <h1>Hi people</h1>
       <ul className="listing">{edges}</ul>
       <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
+      <p>Now go build something great!</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
